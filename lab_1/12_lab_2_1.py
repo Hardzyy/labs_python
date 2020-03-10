603cdd7ae354f74641e89f37b2c8f345dddde386
Mikita <nikita1597538426@gmail.com>
2020-03-10 19:50:46 +0300

    func reauires to fix

--- lab_1/12_lab_2_1.py
+++ lab_1/12_lab_2_1.py
@@ -1,15 +1,9 @@
 import time
 import random
-import math
-
 
 print("Enter array size")
 nArray = int(input())
 start_time = time.time()
-# a = [random.randint(0, nArray) for i in range(nArray)]
-#print(a)
-
-
 a = []
 for i in range(0, nArray):
     a.append(i)
@@ -21,25 +15,36 @@ def elementSum(index1, index2):
         sum += a[i]
     return sum
 
-#print("Time",time.time()-start_time,"secs")
-#n = int(input())
-#for i in range(1):
-#    rightBorder = random.randint(0,nArray-1)
-#    leftBorder = random.randint(0,rightBorder)
-#    print("leftBorder: ", leftBorder, "rightBorder:", rightBorder)
-#    print(elementSum(leftBorder, rightBorder)==1)
-#    print("Step:", i)
-#print("Time",time.time()-start_time,"secs")
+# print("Time",time.time()-start_time,"secs")
+# n = int(input())
+# for i in range(1):
+#     rightBorder = random.randint(0,nArray-1)
+#     leftBorder = random.randint(0,rightBorder)
+#     print("leftBorder: ", leftBorder, "rightBorder:", rightBorder)
+#     print(elementSum(leftBorder, rightBorder)==1)
+#     print("Step:", i)
+# print("Time",time.time()-start_time,"secs")
 
-sqrt_len = int(nArray**0.5 + 1)
-print(sqrt_len)
+def sqrtSum(left, right):
+    sum = 0
+    sqrt_len = int(nArray**0.5 + 1)
+    b = []
+    for i in range(0,int(nArray/sqrt_len + 1)):
+        b.append(0)
 
-b = []
-for i in range(0,int(nArray/sqrt_len)):
-    b.append(0)
-
-for i in range(nArray):
-    n = int(i / sqrt_len)
-    b[n] += a[i]
-
-print(b)
+    for i in range(nArray):
+        n = int(i / sqrt_len)
+        b[n] += a[i]
+    block_left = left/sqrt_len
+    block_right = right/sqrt_len
+    if block_left == block_right:
+        for i in range(left, right+1):
+            sum += a[i]
+    else: for i in range(left, (block_right + 1)*(sqrt_len - 1)):
+              sum += a[i]
+          for i in range(block_left+1, block_right-1)
+              sum += b[i]
+          for i in range(block_right, right):
+              sum += a[i]
+    return sum
+print(sqrtSum(1,2))
