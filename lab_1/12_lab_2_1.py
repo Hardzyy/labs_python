1df3a1eff88cc3628b84e29aec45957de448309c
Mikita <nikita1597538426@gmail.com>
2020-03-14 16:40:29 +0300

    added enter control

--- lab_1/12_lab_2_1.py
+++ lab_1/12_lab_2_1.py
@@ -1,4 +1,3 @@
-import time
 import random
 import math
 
@@ -20,51 +19,59 @@ def elementSum(index1, index2):
         sum += a[i]
     return sum
 
-print("Enter array size")
-nArray = 10000
-start_time = time.time()
+print('To stop array print STOP.')
+print("Enter array:")
 a = []
-for i in range(0, nArray):
-    a.append(i)
-print(a)
+truth = 1
+while truth:
+	c = input()
+	if c == "stop" or c == "STOP":
+		truth = 0
+	else: 
+		try:
+			a.append(int(c))
+		except:
+			print("Sorry, but you enter wrong one, plese try again or enter STOP")
+
+print('Your array:', a)
 
 b = []
-sqrt_len = math.ceil(nArray**0.5)
+sqrt_len = math.ceil(len(a)**0.5)
 print(sqrt_len)
-for i in range(0,math.ceil(nArray/sqrt_len)):
+for i in range(0,math.ceil(len(a)/sqrt_len)):
     b.append(0)
 
-for i in range(nArray):
+for i in range(len(a)):
     n = int(i / sqrt_len)
     b[n] += a[i]
 print(b)
 
-for i in range(10000):
-    rightBorder = random.randint(0,nArray-1)
-    leftBorder = random.randint(0,rightBorder)
-    print("leftBorder:", leftBorder)
-    print("rightBorder:", rightBorder)
-    print("elementSum:", elementSum(leftBorder,rightBorder))
-    print("sqrtSum:", sqrtSum(leftBorder,rightBorder))
-    if elementSum(leftBorder, rightBorder) == sqrtSum(leftBorder, rightBorder): 
-        print("good")
-    else: break 
-    print("Step:", i)
+# for i in range(10000):
+#     rightBorder = random.randint(0,len(a)-1)
+#     leftBorder = random.randint(0,rightBorder)
+#     print("leftBorder:", leftBorder)
+#     print("rightBorder:", rightBorder)
+#     print("elementSum:", elementSum(leftBorder,rightBorder))
+#     print("sqrtSum:", sqrtSum(leftBorder,rightBorder))
+#     if elementSum(leftBorder, rightBorder) == sqrtSum(leftBorder, rightBorder): 
+#         print("good")
+#     else: break 
+#     print("Step:", i)
 
 
 # for i in range(10000):
-#     rightBorder = random.randint(0,nArray-1)
+#     rightBorder = random.randint(0,len(a)-1)
 #     leftBorder = random.randint(0,rightBorder)
 #     print("sqrtSum:", sqrtSum(leftBorder,rightBorder))
 
 # for i in range(10000):
-#     rightBorder = random.randint(0,nArray-1)
+#     rightBorder = random.randint(0,len(a)-1)
 #     leftBorder = random.randint(0,rightBorder)
 #     print("sqrtSum:", elementSum(leftBorder,rightBorder))
 
 
 
-print("Time",time.time()-start_time,"secs")
+
 
 
 
