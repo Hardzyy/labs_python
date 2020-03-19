6f6c052015dea17ac836717beb98af179f928bc2
Mikita <nikita1597538426@gmail.com>
2020-03-19 11:24:43 +0300

    some best practices

--- lab_2/12_lab_2_4.py
+++ lab_2/12_lab_2_4.py
@@ -1,5 +1,20 @@
-a = [1,2,23,[2,5,[6,4]],1,1]
+a = [1,2,23,[2,5,[6,4,123,1,23,1,231,2]],1,[12,31,12312,[12,123,123,3]],1]
 
-for i in range(len(a)):
-	if type(a[i]) == list:
-		 
+
+def flatten_it(a):
+	for i in range(len(a)):
+		if type(a[i]) is list:
+			b = a[i]
+			del a[i]
+			for k in range(len(b)):
+				a.insert(i+k, b[k])
+		count = 0
+		if type(a[i]) is not list:
+			count += 1
+		if count == len(a):
+			return a
+	return a
+
+print(flatten_it(a))
+aa = flatten_it(a)
+print(flatten_it(aa), len(aa))		
