27a1d3330e0b11da40a0b850816bf4f64934efec
Mikita <nikita1597538426@gmail.com>
2020-03-20 17:10:43 +0300

    added for lists

--- lab_2/12_lab_2_4.py
+++ lab_2/12_lab_2_4.py
@@ -1,20 +1,30 @@
-a = [1,2,23,[2,5,[6,4,123,1,23,1,231,2]],1,[12,31,12312,[12,123,123,3]],1]
+a = [1,2,23,[2,5,[6,4,123,1], 1231, 123,3123,[123123,123,123,[1231,123,[123,2]]]],23,1,231,2]
+print(a)
 
 
 def flatten_it(a):
+	type_a = type(a)
 	for i in range(len(a)):
-		if type(a[i]) is list:
+		if type(a[i]) is type_a:
 			b = a[i]
 			del a[i]
 			for k in range(len(b)):
 				a.insert(i+k, b[k])
 		count = 0
-		if type(a[i]) is not list:
-			count += 1
+		for i in range(len(a)):
+			if type(a[i]) is not type_a:
+				count += 1
 		if count == len(a):
 			return a
-	return a
+	print(a)
+	return flatten_it(a)
 
 print(flatten_it(a))
-aa = flatten_it(a)
-print(flatten_it(aa), len(aa))		
+
+# type_a = type(a)
+# for i in range(len(a)):
+# 	if type(a[i]) is type_a:
+# 		b = a[i]
+# 		for k in range(len(b)):
+# 			a.insert(i+k, b[k])
+# print(a)
