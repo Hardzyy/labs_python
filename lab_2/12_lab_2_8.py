dc8119d49c21221cd4b43b337e9e83106bd16c51
Mikita <nikita1597538426@gmail.com>
2020-03-23 16:43:02 +0300

    fix

--- lab_2/12_lab_2_8.py
+++ lab_2/12_lab_2_8.py
@@ -1,17 +1,20 @@
 def exp_test(num):
-	exponent = 0
-	while num:
-		if num == 1:
-			break
-		if num % 2 == 0:
-			exponent += 1
-			num /= 2
-		else: 
-			exponent = 0
-			break
-	if exponent != 0:
-		return exponent
-	else: 
-		return "undefinite"
+    exponent = 0
+    while num:
+        if num == 1 or num == -1:
+            break
+        if num % 2 == 0:
+            if num > 0:
+                exponent += 1
+            else:
+                exponent -= 1            
+            num /= 2
+        else: 
+            exponent = 0
+            break
+    if exponent != 0:
+        return exponent
+    else: 
+        return "undefinite"
 
-print("Exponent is", exp_test(17))
+print("Exponent is", exp_test(-16))
