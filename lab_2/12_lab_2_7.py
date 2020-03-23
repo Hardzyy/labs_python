1c009ff194fa1eda6a14e92703c753cf66bea2f3
Mikita <nikita1597538426@gmail.com>
2020-03-23 16:35:58 +0300

    algo is done

---
+++ lab_2/12_lab_2_7.py
@@ -0,0 +1,10 @@
+def Leonardo_num(n):
+    if n == 0 or n == 1:
+        return 1
+    fi = (1 + 5**0.5)*0.5
+    phi = (1 - 5**0.5)*0.5
+    leo = (2 / 5**0.5) * (fi**(n+1) - phi**(n-1)) - 1 
+    return round(leo)
+
+for i in range(1000):
+    print(Leonardo_num(i))
