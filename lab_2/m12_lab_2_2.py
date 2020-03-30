44da8324df23430a4ead5a3acf935a21ab2334a2
Mikita <nikita1597538426@gmail.com>
2020-03-30 17:01:30 +0300

    pep8

--- lab_2/m12_lab_2_2.py
+++ lab_2/m12_lab_2_2.py
@@ -3,6 +3,7 @@ import progressbar
 import argparse
 import time
 
+
 def check_dimension(array):
     if len(array) != 2:
         print("Wrong tuple")
@@ -13,53 +14,55 @@ def check_dimension(array):
 def check_tuple(value):
     ivalue = int(value)
     if ivalue <= 0:
-        raise argparse.ArgumentTypeError("%s is an invalid positive int value" % value)
+        raise argparse.ArgumentTypeError("%s is an invalid"
+                                         "positive int value" % value)
     return ivalue
 
 
 def check_size(value):
     fvalue = float(value)
     if fvalue <= 0:
-        raise argparse.ArgumentTypeError("%s is an invalid positive float value" % value)
+        raise argparse.ArgumentTypeError("%s is an invalid"
+                                         "positive float value" % value)
     return fvalue
 
 
 parser = argparse.ArgumentParser(description='Interactive input')
 parser.add_argument(
     '-n',
-    default = 1,
-    type = check_size,
-    help = 'Size of a file(default: 1Mb)'
+    default=1,
+    type=check_size,
+    help='Size of a file(default: 1Mb)'
 )
 parser.add_argument(
     '--k',
     nargs='+',
-    type = check_tuple,
-    default = (10, 100),
-    help = 'Amount of words in a string(default: (10, 100))'
+    type=check_tuple,
+    default=(10, 100),
+    help='Amount of words in a string(default: (10, 100))'
 )
-    
+
 parser.add_argument(
     '--l',
     nargs='+',
-    type = check_tuple,
-    default = (3, 10),
-    help = 'Length of a word(default: (3, 10))'
+    type=check_tuple,
+    default=(3, 10),
+    help='Length of a word(default: (3, 10))'
 )
 
 args = parser.parse_args()
 k = tuple(args.k)
 kk = []
-for i in range(k[0],k[1]+1):
+for i in range(k[0], k[1]+1):
     kk.append(i)
 check_dimension(k)
 
 l = tuple(args.l)
 ll = []
-for i in range(l[0],l[1]+1):
+for i in range(l[0], l[1]+1):
     ll.append(i)
 check_dimension(l)
-    
+
 file = open("12_lab_2_2_output.txt", "w")
 
 alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
@@ -80,13 +83,13 @@ bar = progressbar.ProgressBar(maxval=int(1024**2 * n + 1), widgets=[
 start_time = time.time()
 
 while x <= int(1024**2 * n - 1):
-    if x != 0 and spaces !=0 and (spaces % (string_len) == 0):
-        spaces = 0 
+    if x != 0 and spaces != 0 and (spaces % (string_len) == 0):
+        spaces = 0
         file.write('\n')
         x += 2
         string_len = random.choice(kk)
     file.write(random.choice(alphabet))
-    x += 1        
+    x += 1
     str_help += 1
     if str_help % length == 0:
         spaces += 1
@@ -97,5 +100,5 @@ while x <= int(1024**2 * n - 1):
     bar.update(x)
 
 bar.finish()
-file.close() 
+file.close()
 print("Total time complexity", (time.time() - start_time))
