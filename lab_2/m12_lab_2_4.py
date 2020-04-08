1a001267d3d898b0a3f9eac9758deb944c766e6a
Mikita <nikita1597538426@gmail.com>
2020-04-08 18:29:54 +0300

    something

--- lab_2/m12_lab_2_4.py
+++ lab_2/m12_lab_2_4.py
@@ -1,10 +1,6 @@
 import argparse
 
 
-# a = [1, 2, 23, [2, 5, [6, 4, 123, 1], 1231, 123, 3123, [123123, 123, 123, [1231, 123, [123, 2]]]], 23, 1, 231, 2]
-# print(a)
-
-
 def flatten_it(a):
     type_a = type(a)
     for i in range(len(a)):
@@ -21,9 +17,12 @@ def flatten_it(a):
             return a
     return flatten_it(a)
 
-parser = argparse.ArgumentParser(description='It helps me to do lab')
-parser.add_argument('-a')
-args = parser.parse_args()
-array = args.a
-a = [int(elem) for elem in array.strip().split(',')]
-print(a)
+# parser = argparse.ArgumentParser(description='It helps me to do lab')
+# parser.add_argument('--f',
+#     help ="Enter JSON file name"
+#     )
+# args = parser.parse_args()
+# array = args.a
+# print(a)
+
+array = [array, 12,312, array]
