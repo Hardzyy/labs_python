386b4b1ed5c203330088c1efda1252b7b1b585b9
Mikita <nikita1597538426@gmail.com>
2020-04-08 18:48:49 +0300

    name func changed

--- lab_2/m12_lab_2_2.py
+++ lab_2/m12_lab_2_2.py
@@ -4,14 +4,14 @@ import argparse
 import time
 
 
-def check_dimension(array):
+def checkDimension(array):
     if len(array) != 2:
         print("Wrong tuple")
         print("Program is ended")
         quit()
 
 
-def check_tuple(value):
+def checkTuple(value):
     ivalue = int(value)
     if ivalue <= 0:
         raise argparse.ArgumentTypeError("%s is an invalid"
@@ -19,7 +19,7 @@ def check_tuple(value):
     return ivalue
 
 
-def check_size(value):
+def checkSize(value):
     fvalue = float(value)
     if fvalue <= 0:
         raise argparse.ArgumentTypeError("%s is an invalid"
@@ -31,13 +31,13 @@ parser = argparse.ArgumentParser(description='Interactive input')
 parser.add_argument(
     '-n',
     default=1,
-    type=check_size,
+    type=checkSize,
     help='Size of a file(default: 1Mb)'
 )
 parser.add_argument(
     '--k',
     nargs='+',
-    type=check_tuple,
+    type=checkTuple,
     default=(10, 100),
     help='Amount of words in a string(default: (10, 100))'
 )
@@ -45,7 +45,7 @@ parser.add_argument(
 parser.add_argument(
     '--l',
     nargs='+',
-    type=check_tuple,
+    type=checkTuple,
     default=(3, 10),
     help='Length of a word(default: (3, 10))'
 )
@@ -55,13 +55,13 @@ k = tuple(args.k)
 rand_for_str = []
 for i in range(k[0], k[1]+1):
     rand_for_str.append(i)
-check_dimension(k)
+checkDimension(k)
 
 l = tuple(args.l)
 rand_for_word = []
 for i in range(l[0], l[1]+1):
     rand_for_word.append(i)
-check_dimension(l)
+checkDimension(l)
 
 file = open("output.txt", "w")
 
@@ -71,7 +71,7 @@ n = args.n
 length = random.choice(rand_for_word)
 string_len = random.choice(rand_for_str)
 spaces = 0
-x = 0
+size_in_bytes = 0
 str_help = 0
 
 bar = progressbar.ProgressBar(maxval=int(1024**2 * n + 1), widgets=[
@@ -82,22 +82,22 @@ bar = progressbar.ProgressBar(maxval=int(1024**2 * n + 1), widgets=[
 
 start_time = time.time()
 
-while x < int(1024**2 * n):
-    if x != 0 and spaces != 0 and (spaces % (string_len) == 0):
+while size_in_bytes < int(1024**2 * n):
+    if size_in_bytes != 0 and spaces != 0 and (spaces % (string_len) == 0):
         spaces = 0
         file.write('\n')
-        x += 2
+        size_in_bytes += 2
         string_len = random.choice(rand_for_str)
     file.write(random.choice(alphabet))
-    x += 1
+    size_in_bytes += 1
     str_help += 1
     if str_help % length == 0:
         spaces += 1
         file.write(' ')
-        x += 1
+        size_in_bytes += 1
         length = random.choice(rand_for_word)
         str_help = 0
-    bar.update(x)
+    bar.update(size_in_bytes)
 
 bar.finish()
 file.close()
