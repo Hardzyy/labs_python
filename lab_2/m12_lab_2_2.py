e7f6d20c31e5939d0710a2392aa9fcf7804f4bb4
Mikita <nikita1597538426@gmail.com>
2020-04-11 19:33:37 +0300

    module

--- lab_2/m12_lab_2_2.py
+++ lab_2/m12_lab_2_2.py
@@ -27,78 +27,83 @@ def checkSize(value):
     return fvalue
 
 
-parser = argparse.ArgumentParser(description='Interactive input')
-parser.add_argument(
-    '-n',
-    default=1,
-    type=checkSize,
-    help='Size of a file(default: 1Mb)'
-)
-parser.add_argument(
-    '--k',
-    nargs='+',
-    type=checkTuple,
-    default=(10, 100),
-    help='Amount of words in a string(default: (10, 100))'
-)
+def main():
+    parser = argparse.ArgumentParser(description='Interactive input')
+    parser.add_argument(
+        '-n',
+        default=1,
+        type=checkSize,
+        help='Size of a file(default: 1Mb)'
+    )
+    parser.add_argument(
+        '--k',
+        nargs='+',
+        type=checkTuple,
+        default=(10, 100),
+        help='Amount of words in a string(default: (10, 100))'
+    )
 
-parser.add_argument(
-    '--l',
-    nargs='+',
-    type=checkTuple,
-    default=(3, 10),
-    help='Length of a word(default: (3, 10))'
-)
+    parser.add_argument(
+        '--l',
+        nargs='+',
+        type=checkTuple,
+        default=(3, 10),
+        help='Length of a word(default: (3, 10))'
+    )
 
-args = parser.parse_args()
-k = tuple(args.k)
-rand_for_str = []
-for i in range(k[0], k[1]+1):
-    rand_for_str.append(i)
-checkDimension(k)
+    args = parser.parse_args()
+    k = tuple(args.k)
+    rand_for_str = []
+    for i in range(k[0], k[1]+1):
+        rand_for_str.append(i)
+    checkDimension(k)
 
-l = tuple(args.l)
-rand_for_word = []
-for i in range(l[0], l[1]+1):
-    rand_for_word.append(i)
-checkDimension(l)
+    l = tuple(args.l)
+    rand_for_word = []
+    for i in range(l[0], l[1]+1):
+        rand_for_word.append(i)
+    checkDimension(l)
 
-file = open("output.txt", "w")
+    file = open("output.txt", "w")
 
-alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-n = args.n
-length = random.choice(rand_for_word)
-string_len = random.choice(rand_for_str)
-spaces = 0
-size_in_bytes = 0
-str_help = 0
+    n = args.n
+    length = random.choice(rand_for_word)
+    string_len = random.choice(rand_for_str)
+    spaces = 0
+    size_in_bytes = 0
+    str_help = 0
 
-bar = progressbar.ProgressBar(maxval=int(1024**2 * n + 1), widgets=[
-    'Working...: ',
-    progressbar.Bar(left='[', marker='*', right=']'),
-    progressbar.Percentage(),
-]).start()
+    bar = progressbar.ProgressBar(maxval=int(1024**2 * n + 1), widgets=[
+        'Working...: ',
+        progressbar.Bar(left='[', marker='*', right=']'),
+        progressbar.Percentage(),
+    ]).start()
 
-start_time = time.time()
+    start_time = time.time()
 
-while size_in_bytes < int(1024**2 * n):
-    if size_in_bytes != 0 and spaces != 0 and (spaces % (string_len) == 0):
-        spaces = 0
-        file.write('\n')
-        size_in_bytes += 2
-        string_len = random.choice(rand_for_str)
-    file.write(random.choice(alphabet))
-    size_in_bytes += 1
-    str_help += 1
-    if str_help % length == 0:
-        spaces += 1
-        file.write(' ')
+    while size_in_bytes < int(1024**2 * n):
+        if size_in_bytes != 0 and spaces != 0 and (spaces % (string_len) == 0):
+            spaces = 0
+            file.write('\n')
+            size_in_bytes += 2
+            string_len = random.choice(rand_for_str)
+        file.write(random.choice(alphabet))
         size_in_bytes += 1
-        length = random.choice(rand_for_word)
-        str_help = 0
-    bar.update(size_in_bytes)
+        str_help += 1
+        if str_help % length == 0:
+            spaces += 1
+            file.write(' ')
+            size_in_bytes += 1
+            length = random.choice(rand_for_word)
+            str_help = 0
+        bar.update(size_in_bytes)
 
-bar.finish()
-file.close()
-print("Total time complexity", (time.time() - start_time))
+    bar.finish()
+    file.close()
+    print("Total time complexity", (time.time() - start_time))
+
+
+if __name__ == "__main__":
+    main()
