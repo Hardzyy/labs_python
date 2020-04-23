6a1af361f64493e2414a57c3ed3a6fb1d7c5f7cd
Mikita <nikita1597538426@gmail.com>
2020-04-23 11:50:37 +0300

    zxc

--- lab_2/m12_lab_2_4.py
+++ lab_2/m12_lab_2_4.py
@@ -1,7 +1,7 @@
 import argparse
 
 
-def flatten_it(a):
+def flatten_list(a):
     type_a = type(a)
     for i in range(len(a)):
         if type(a[i]) is type_a:
@@ -17,12 +17,41 @@ def flatten_it(a):
             return a
     return flatten_it(a)
 
-# parser = argparse.ArgumentParser(description='It helps me to do lab')
-# parser.add_argument('--f',
-#     help ="Enter JSON file name"
-#     )
-# args = parser.parse_args()
-# array = args.a
-# print(a)
 
-array = [array, 12,312, array]
+def flatten_tuple(a):
+    list_a = list(a)
+    for i in range(len(list_a)):
+        if type(list_a[i]) is tuple:
+            helper = list(list_a[i])
+            del list_a[i]
+            for k in range(len(helper)):
+                list_a.insert(i+k, helper[k])
+        count = 0
+        for i in range(len(list_a)):
+            if type(list_a[i]) is not tuple:
+                count += 1
+        if count == len(list_a):
+            return tuple(list_a)
+    return flat_tuple(tuple(list_a))
+
+
+def flatten_it(a):
+    if type(a) is list:
+        return flatten_list(a)
+    if type(a) is tuple:
+        return flatten_tuple(a)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='It helps me to do lab')
+    parser.add_argument('--obj',
+        type=str,
+        help="Enter an object to flatten."
+        )
+    
+    args = parser.parse_args()
+    array = eval(args.obj)
+    print(flatten_it(array))
+
+if __name__ == "__main__":
+    main()
