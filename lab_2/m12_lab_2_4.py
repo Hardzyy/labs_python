e96ea0420ddec2ccafdb3a464c71680a0ab132a3
Mikita <nikita1597538426@gmail.com>
2020-04-24 12:46:45 +0300

    some fixes

--- lab_2/m12_lab_2_4.py
+++ lab_2/m12_lab_2_4.py
@@ -1,17 +1,33 @@
 import argparse
 
 
-def flatten_list(a):
+def for_check(arr_type, types):
+    return any([(arr_type is elem) for elem in types])
+
+
+
+def check(a):
     type_a = type(a)
+    list_a = list(a)
+    types = [type(a), int, str, float, bool]
+    for i in range(len(list_a)):
+        if for_check(type(list_a[i]), types):
+            pass
+        else:
+            raise ValueError('Your object cotsists of several types of structures.')            
+
+
+def flatten_list(a):
+    check(a)
     for i in range(len(a)):
-        if type(a[i]) is type_a:
+        if type(a[i]) is list:
             b = a[i]
             del a[i]
             for k in range(len(b)):
                 a.insert(i+k, b[k])
         count = 0
         for i in range(len(a)):
-            if type(a[i]) is not type_a:
+            if type(a[i]) is not list:
                 count += 1
         if count == len(a):
             return a
@@ -19,6 +35,7 @@ def flatten_list(a):
 
 
 def flatten_tuple(a):
+    check(a)
     list_a = list(a)
     for i in range(len(list_a)):
         if type(list_a[i]) is tuple:
@@ -40,18 +57,23 @@ def flatten_it(a):
         return flatten_list(a)
     if type(a) is tuple:
         return flatten_tuple(a)
+    else:
+        print('Only lists and tuple are used')
 
 
 def main():
-    parser = argparse.ArgumentParser(description='It helps me to do lab')
+    parser = argparse.ArgumentParser(description='Created to add objects')
     parser.add_argument('--obj',
+        default='[1,2,[3,4,5],[6,[7,8]]]',
         type=str,
         help="Enter an object to flatten."
         )
-    
+
     args = parser.parse_args()
     array = eval(args.obj)
-    print(flatten_it(array))
+    print('Original object', array)
+    print('Flattened object', flatten_it(array))
+
 
 if __name__ == "__main__":
     main()
