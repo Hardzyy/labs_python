432f39ecd9ffa05a8a5249cba93ea9f80cb90088
Mikita <nikita1597538426@gmail.com>
2020-04-24 12:55:12 +0300

    pep8 confirmed

--- lab_2/m12_lab_2_4.py
+++ lab_2/m12_lab_2_4.py
@@ -5,7 +5,6 @@ def for_check(arr_type, types):
     return any([(arr_type is elem) for elem in types])
 
 
-
 def check(a):
     type_a = type(a)
     list_a = list(a)
@@ -14,17 +13,18 @@ def check(a):
         if for_check(type(list_a[i]), types):
             pass
         else:
-            raise ValueError('Your object cotsists of several types of structures.')            
+            raise ValueError('Your object cotsists',
+                             'of several types of structures.')
 
 
 def flatten_list(a):
     check(a)
     for i in range(len(a)):
         if type(a[i]) is list:
-            b = a[i]
+            sub_a = a[i]
             del a[i]
-            for k in range(len(b)):
-                a.insert(i+k, b[k])
+            for k in range(len(sub_a)):
+                a.insert(i+k, sub_a[k])
         count = 0
         for i in range(len(a)):
             if type(a[i]) is not list:
@@ -39,10 +39,10 @@ def flatten_tuple(a):
     list_a = list(a)
     for i in range(len(list_a)):
         if type(list_a[i]) is tuple:
-            helper = list(list_a[i])
+            sub_a = list(list_a[i])
             del list_a[i]
-            for k in range(len(helper)):
-                list_a.insert(i+k, helper[k])
+            for k in range(len(sub_a)):
+                list_a.insert(i+k, sub_a[k])
         count = 0
         for i in range(len(list_a)):
             if type(list_a[i]) is not tuple:
@@ -64,10 +64,10 @@ def flatten_it(a):
 def main():
     parser = argparse.ArgumentParser(description='Created to add objects')
     parser.add_argument('--obj',
-        default='[1,2,[3,4,5],[6,[7,8]]]',
-        type=str,
-        help="Enter an object to flatten."
-        )
+                        default='[1, 2, [3, 4, 5], [6, [7, 8]]]',
+                        type=str,
+                        help="Enter an object to flatten it."
+                        )
 
     args = parser.parse_args()
     array = eval(args.obj)
