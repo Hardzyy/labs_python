b39d15879bb6e77616083b523759f81d5e1e9d77
Mikita <nikita1597538426@gmail.com>
2020-05-02 12:33:04 +0300

    some adds

--- lab_2/m12_lab_2_5.py
+++ lab_2/m12_lab_2_5.py
@@ -17,9 +17,37 @@ def str_to_str(string):
     return "\"" + string + "\""
 
 
-def list_to_str(li):
-    string = ','.join(str(elem) for elem in li)
-    return '[{}]'.format(string)
+def bool_to_str(bl):
+    if bl:
+        return 'true'
+    else:
+        return 'false'
 
-aa = [1,1,1]
+
+def float_to_str(fl):
+    return str(fl)
+
+
+def list_to_str(li):
+    string = ''
+    for i in range(len(li)):
+        if type(li[i]) in (list, tuple):
+            string += list_to_str(li[i]) + ', '
+        if type(li[i]) is int:
+            string += int_to_str(li[i]) + ', '
+        if type(li[i]) is str:
+            string += str_to_str(li[i]) + ', '
+        if type(li[i]) is float:
+            string += str(li[i]) + ', '
+        if type(li[i]) is type(None):
+            string += "null" + ', '
+        if type(li[i]) is bool:
+            string += bool_to_str(li[i]) + ', '
+    return '[' + string[:-2] + ']'
+
+
+def dict_to_str(di):
+
+
+aa = [1,2.2,'ass', True, None, False, [1,1]]
 print(list_to_str(aa))
