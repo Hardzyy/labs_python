94a07dfe733df4cd0d6d82ad890ddff602d6c09e
Mikita <nikita1597538426@gmail.com>
2020-05-02 14:18:31 +0300

    one more change

--- lab_2/m12_lab_2_6.py
+++ lab_2/m12_lab_2_6.py
@@ -1,17 +1,18 @@
-file = open('file.txt', 'r')
-aa = file.readline()
-file.close()
-print(aa)
 
 def json_to_object(string):
 	string = string.strip()
+	if string = 'null':
+		return None
+	if string = 'false':
+		return False
+	if string = 'true':
+		return True
 	if string[0] == "'" and string[-1] == "'":
 		return json_to_str(string)
 	if string.count('.') == 1 and string.count(' ') == 0:
 		return float(string)
 	if string[0] == "[" and string[-1] == "]":
 		return json_to_list(string)
-
 
 
 def json_to_str(obj):
@@ -22,5 +23,11 @@ def json_to_list(obj):
 	string = obj[1:-1].split(', ')
 	array = []
 	for i in range(len(string)):
-		array.extend(json_to_object(string))
+		array.append(json_to_object(string[i]))
 	return array
+
+
+f = open('111.txt', 'r')
+for i in range(3):
+	obj = json_to_object(f.readline())
+	print(obj, type(obj))
